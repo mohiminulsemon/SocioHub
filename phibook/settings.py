1ca593d40c055d109e4439a746901e5aa60f4e09
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-01-17 15:52:06 +0600

    upadated name

--- phibook/settings.py
+++ phibook/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://phibook.onrender.com', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://sociohub-0kd3.onrender.com', 'https://*.127.0.0.1']
 
 # Application definition
 
