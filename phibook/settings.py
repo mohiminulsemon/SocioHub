1a88fe0c0dbc63e51b5e0d62e37eea1cfb996fd9
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-01-23 00:00:44 +0600

    registration updated

--- phibook/settings.py
+++ phibook/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_URL = '/media/'
 
 # Default primary key field type
