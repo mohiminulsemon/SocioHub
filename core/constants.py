bf7924f9bcb86715051e040f723b485d680f0909
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-01-24 03:15:32 +0600

    updated styles

---
+++ core/constants.py
@@ -0,0 +1,12 @@
+# constants.py
+USER_SPOTLIGHT_DATA = [
+    {"title": "User 1", "image": "user1.gif"},
+    {"title": "User 2", "image": "user2.gif"},
+    # Add more user data as needed
+]
+
+LATEST_NEWS_DATA = [
+    {"title": "News 1", "image": "news1.gif"},
+    {"title": "News 2", "image": "news2.gif"},
+    # Add more news data as needed
+]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from Posts.models import Post, Comment
 from Posts import forms
+from .constants import USER_SPOTLIGHT_DATA, LATEST_NEWS_DATA
 # Create your views here.
 
 
 
 def home(request):
     posts = Post.objects.all()
-    comments = Comment.objects.all()  # Fetch all comments
+    comments = Comment.objects.all()
     comment_form = forms.CommentForm()
-    return render(request, 'index.html', {'posts': posts, 'comments': comments, 'comment_form': comment_form})
+
+    # Fetch data for UserSpotlight and Latest News from constants.py
+    user_spotlight_data = USER_SPOTLIGHT_DATA
+    latest_news_data = LATEST_NEWS_DATA
+
+    context = {
+        'posts': posts,
+        'comments': comments,
+        'comment_form': comment_form,
+        'user_spotlight_data': user_spotlight_data,
+        'latest_news_data': latest_news_data,
+    }
+
+    return render(request, 'index.html', context)
